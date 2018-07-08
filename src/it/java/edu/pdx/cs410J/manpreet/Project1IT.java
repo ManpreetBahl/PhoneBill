20b4608d323152e213f2d7bba212976e00fa0dbe
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-07-07 18:28:13 -0700

    Command parser V1, still performing tests

--- src/it/java/edu/pdx/cs410J/manpreet/Project1IT.java
+++ src/it/java/edu/pdx/cs410J/manpreet/Project1IT.java
@@ -29,4 +29,96 @@ public class Project1IT extends InvokeMainTestCase {
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
 
+  @Test
+  public void testREADME(){
+      MainMethodResult result = invokeMain("-README");
+      assertThat(result.getExitCode(), equalTo(0));
+      assertThat(result.getTextWrittenToStandardOut(), containsString(String.join(
+              System.getProperty("line.separator"),
+              "Assignment: Project 1",
+              "Author: Manpreet Bahl",
+              "This project implements the fundamental design of the PhoneBill application.",
+              "Specifically, this project implements the PhoneCall class which contains",
+              "information about one call: caller phone number, callee phone number, start",
+              "and end time of the phone call.",
+              "The PhoneBill class contains all the PhoneCalls for a particular customer to",
+              "billed. By designing and testing these two fundamental classes, we can be sure",
+              "these will work as intended even if the way they are used is different from",
+              "project to project.",
+              "usage: java edu.pdx.cs410J.manpreet.Project1 [options] <args>",
+              "args are (in this order):",
+              "  customer               Person whose phone bill we’re modeling",
+              "  callerNumber           Phone number of caller",
+              "  calleeNumber           Phone number of person who was called",
+              "  startTime              Date and time call began (24-hour time)",
+              "  endTime                Date and time call ended (24-hour time)",
+              "options are (options may appear in any order):",
+              "  -print                 Prints a description of the new phone call",
+              "  -README                Prints a README for this project and exits",
+              "Date and time should be in the format: mm/dd/yyyy hh:mm"
+      )));
+  }
+
+  @Test
+  public void testMissingCustomerName(){
+      MainMethodResult result = invokeMain("-print");
+      assertThat(result.getExitCode(),equalTo(1));
+      assertThat(result.getTextWrittenToStandardError(), containsString("Missing customer name"));
+  }
+
+  @Test
+  public void testMissingCallerNum(){
+      MainMethodResult result = invokeMain("-print", "customer");
+      assertThat(result.getExitCode(),equalTo(1));
+      assertThat(result.getTextWrittenToStandardError(), containsString("Missing caller phone number"));
+  }
+
+  @Test
+  public void testMissingCalleeNum(){
+    MainMethodResult result = invokeMain("-print", "customer", "123-456-7890");
+    assertThat(result.getExitCode(),equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing callee phone number"));
+  }
+
+  @Test
+  public void testMissingStartTime(){
+    MainMethodResult result = invokeMain("-print", "customer", "123-456-7890", "123-456-7899");
+    assertThat(result.getExitCode(),equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing start time of the phone call"));
+  }
+
+  @Test
+  public void testMissingEndTime(){
+    MainMethodResult result = invokeMain("-print", "customer", "123-456-7890", "123-456-7899", "1/2/2301", "3:00");
+    assertThat(result.getExitCode(),equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing end time of the phone call"));
+  }
+
+  @Test
+  public void testUnknownCommand(){
+    MainMethodResult result = invokeMain("-print", "customer", "-123-456-7890", "123-456-7899");
+    assertThat(result.getExitCode(),equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Invalid command: -123-456-7890"));
+  }
+
+  @Test
+  public void testPhoneCallValidationBadCallerNum(){
+    MainMethodResult result = invokeMain("customer", "123-456-7H90", "123-456-7899", "1/2/2301", "3:00", "2/3/2402", "23:00");
+    assertThat(result.getExitCode(),equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Invalid phone number entered. Phone numbers have the form nnn-nnn-nnnn where n is a number 0-9."));
+  }
+
+  @Test
+  public void testPhoneCallValidationBadCalleeNum(){
+    MainMethodResult result = invokeMain("customer", "123-456-7390", "123-456-899", "1/2/2301", "3:00", "2/3/2402", "23:00");
+    assertThat(result.getExitCode(),equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Invalid phone number entered. Phone numbers have the form nnn-nnn-nnnn where n is a number 0-9."));
+  }
+
+  @Test
+  public void testDisplay(){
+    MainMethodResult result = invokeMain("-print", "customer", "123-456-7890", "123-456-7899", "1/2/2301", "3:00", "2/3/2402", "23:00");
+    assertThat(result.getExitCode(),equalTo(0));
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Phone call from 123-456-7890 to 123-456-7899 from 1/2/2301 3:00 to 2/3/2402 23:00"));
+  }
 }
--- src/main/java/edu/pdx/cs410J/manpreet/Project1.java
+++ src/main/java/edu/pdx/cs410J/manpreet/Project1.java
@@ -2,20 +2,122 @@ package edu.pdx.cs410J.manpreet;
 
 import edu.pdx.cs410J.AbstractPhoneBill;
 
+import java.util.Arrays;
+
 /**
  * The main class for the CS410J Phone Bill Project
  */
+
 public class Project1 {
+  static final String README = String.join(
+    System.getProperty("line.separator"),
+      "Assignment: Project 1",
+      "Author: Manpreet Bahl",
+      "This project implements the fundamental design of the PhoneBill application.",
+      "Specifically, this project implements the PhoneCall class which contains",
+      "information about one call: caller phone number, callee phone number, start",
+      "and end time of the phone call.",
+      "The PhoneBill class contains all the PhoneCalls for a particular customer to",
+      "billed. By designing and testing these two fundamental classes, we can be sure",
+      "these will work as intended even if the way they are used is different from",
+      "project to project.",
+      usage()
+  );
 
   public static void main(String[] args) {
-    /*
-    PhoneCall call = new PhoneCall();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
-    for (String arg : args) {
-      System.out.println(arg);
+    String customer = null;
+    String callerNum = null;
+    String calleeNum = null;
+    String startTime = null;
+    String endTime = null;
+    boolean toPrint = false;
+    int startIndex = 0;
+    PhoneCall toAdd;
+    PhoneBill bill;
+
+    if(args.length == 0){
+      errorAndExit("Missing command line arguments");
     }
-    System.exit(1);
-    */
+
+    if (containsOption(args, "-README")) {
+      System.out.println(README);
+      System.exit(0);
+    }
+
+    if (containsOption(args, "-print")){
+      ++startIndex;
+      toPrint = true;
+    }
+
+    for(int i = startIndex; i < args.length; i++){
+      if(args[i].startsWith("-")){
+        errorAndExit("Invalid command: " + args[i]);
+      }
+      if (customer == null){
+        customer = args[i];
+      } else if (callerNum == null){
+        callerNum = args[i];
+      } else if (calleeNum == null){
+        calleeNum = args[i];
+      } else if (startTime == null){
+        startTime = args[i] + " " + args[++i];
+      } else if (endTime == null) {
+        endTime = args[i] + " " + args[++i];
+      } else{
+        errorAndExit("Too many arguments entered");
+      }
+    }
+
+    if (customer == null){
+      errorAndExit("Missing customer name");
+    } else if (callerNum == null){
+      errorAndExit("Missing caller phone number");
+    } else if (calleeNum == null){
+      errorAndExit("Missing callee phone number");
+    } else if (startTime == null){
+      errorAndExit("Missing start time of the phone call");
+    } else if (endTime == null) {
+      errorAndExit("Missing end time of the phone call");
+    }
+
+    try{
+      toAdd = new PhoneCall(callerNum, calleeNum, startTime,endTime);
+      bill = new PhoneBill(customer);
+      bill.addPhoneCall(toAdd);
+
+      if(toPrint){
+        System.out.println(toAdd.toString());
+      }
+
+      System.exit(0);
+    }catch (IllegalArgumentException ie){
+      errorAndExit(ie.getMessage());
+    }
   }
 
+  private static String usage(){
+    return String.join(
+        System.getProperty("line.separator"),
+        "usage: java edu.pdx.cs410J.manpreet.Project1 [options] <args>",
+        "args are (in this order):",
+        "  customer               Person whose phone bill we’re modeling",
+        "  callerNumber           Phone number of caller",
+        "  calleeNumber           Phone number of person who was called",
+        "  startTime              Date and time call began (24-hour time)",
+        "  endTime                Date and time call ended (24-hour time)",
+        "options are (options may appear in any order):",
+        "  -print                 Prints a description of the new phone call",
+        "  -README                Prints a README for this project and exits",
+        "Date and time should be in the format: mm/dd/yyyy hh:mm"
+    );
+  }
+  private static void errorAndExit(String message){
+    System.err.println(message);
+    System.err.println(usage());
+    System.exit(1);
+  }
+
+  private static boolean containsOption(String[] args, String option) {
+    return Arrays.stream(args).anyMatch(s -> s.equals(option));
+  }
 }
